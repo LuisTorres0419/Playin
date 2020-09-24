bcb58f1181c08d6125fa0d36e589aa402be731d4
luis torres <tl224627@gmail.com>
2020-09-24 08:34:26 -0600

    Add project files.

---
+++ Playin/Playin.vb
@@ -0,0 +1,32 @@
+﻿Module Playin
+
+    Sub Main()
+        TestList()
+        Console.ReadLine()
+    End Sub
+
+    Sub TestList()
+        Dim names As New List(Of String)
+
+        names.Add("Joe")
+        names.Add("Tim")
+        names.Add("Frank")
+        names.Add("Mary")
+        names.Add("Sally")
+
+        Console.WriteLine(names.Count)
+        names.Insert(3, "Bill")
+        Console.WriteLine(names.Item(3))
+        Console.WriteLine(names.Contains("Jimmy"))
+
+        For Each name In names
+            Console.WriteLine(name)
+            Console.WriteLine(names.IndexOf(name))
+
+        Next
+
+        'Console.WriteLine(names.Item())
+    End Sub
+
+
+End Module
